c8b351b3feb908976c6ed7afc88de0965acf9b62
santoshml-lab <santoshkrsbg36@gmail.com>
2026-05-01 12:32:50 +0530

    Add files via upload

---
+++ main .py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+model = joblib.load("model.joblib")
+
+class Input(BaseModel):
+    study_hours: float
+    marks: float
+
+@app.get("/")
+def home():
+    return {"message": "API Running"}
+
+@app.post("/predict")
+def predict(data: Input):
+    result = model.predict([[data.study_hours, data.marks]])[0]
+    return {"result": int(result)}
